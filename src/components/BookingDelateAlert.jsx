007f5aba2fd3e623052389a826f5c247084a94f4
soikat63 <soikat6389@gmail.com>
2026-08-14 20:15:13 +0600

    modifiyed the cancel booking components

--- src/components/BookingDelateAlert.jsx
+++ src/components/BookingDelateAlert.jsx
@@ -6,13 +6,16 @@ import { AlertDialog, Button } from "@heroui/react";
 const BookingDelateAlert = ({ bookingId }) => {
   const handleCancelBooking = async () => {
     const { data: tokenData } = await authClient.token();
-    const res = await fetch(`${process.env.SERVER_URL}/booking/${bookingId}`, {
-      method: "DELETE",
-      headers: {
-        "content-type": "application/json",
-        authorization: `Bearer ${tokenData?.token}`,
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/booking/${bookingId}`,
+      {
+        method: "DELETE",
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${tokenData?.token}`,
+        },
       },
-    });
+    );
     const data = await res.json();
 
     window.location.reload();
